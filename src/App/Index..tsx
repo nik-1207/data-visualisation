e05bb8adf49769b4e7124eb748214731b67856e2
nik-1207 <nmknikhilkashyap@gmail.com>
2022-12-30 15:01:28 +0530

    setup project

---
+++ src/App/Index..tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Bar from '../components/Bar';
+const App: React.FC = () => {
+  return (
+    <div id="app">
+      <Bar />
+    </div>
+  )
+}
+export default App;
---
+++ src/components/ScatterPlot/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ScatterPlot: React.FC = () => {
+    return <>Scatter Plot</>
+}
+
+export default ScatterPlot;
